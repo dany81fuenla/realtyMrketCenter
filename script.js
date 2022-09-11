63aee05eb2261e577756ed71784a30e2e47d7fbc
dany81fuenla <83925462+dany81fuenla@users.noreply.github.com>
2022-09-11 13:32:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const boton = document.getElementById("switch")
+const boton_contenedor = document.getElementById("switch-container")
+const box = document.getElementById("box")
+const text = document.getElementsByClassName("menu-container")
+
+boton.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+})
+        
+    
+
